fe9a62e9bbb5d3775e5a3e71492f7926925bc778
Seunghyun Kim <kimseunghyun.bg@gmail.com>
2020-04-02 00:01:30 +0900

    챕터9 추가 및 완료

---
+++ spring4Introduction/chapter09/src/main/java/chap09/HelloController.java
@@ -0,0 +1,20 @@
+package chap09;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author Seunghyun Kim
+ * @since 2020-04-01
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value = "name", required = false) String name) {
+        model.addAttribute("greeting", "안녕하세요, " + name);
+        return "hello";
+    }
+}
---
+++ spring4Introduction/chapter09/src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: kimse
+  Date: 2020-04-01
+  Time: 오후 11:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+인사말:${greeting}
+</body>
+</html>
